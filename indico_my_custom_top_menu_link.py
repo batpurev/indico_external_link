d756bdfbb629cde308ad9a4b0ce3a657e382da23
batpurev <batpurev@gmail.com>
2021-05-01 22:35:20 +0800

    Update indico_my_custom_top_menu_link.py

--- indico_my_custom_top_menu_link.py
+++ indico_my_custom_top_menu_link.py
@@ -12,6 +12,12 @@ class MyCustomTopMenuLinkPlugin(IndicoPlugin):
     def init(self):
         super(MyCustomTopMenuLinkPlugin, self).init()
         self.connect(signals.menu.items, self._extend_top_menu, sender='top-menu')
+        self.template_hook('page-footer', self._inject_footer_links)
 
     def _extend_top_menu(self, sender, **kwargs):
-        return TopMenuItem('my-menu-item', 'Google', 'https://www.google.com', weight=95)
+        return TopMenuItem('my-menu-item', 'Google', 'https://www.google.com', weight=105)
+    def _inject_footer_links(self, **kwargs):
+        #return (' <a href=\'#\'>aaa</a> | <a href=\'#\'>bbb</a> | <a href=\'#\'>aaa</a> | <a href=\'#\'>ccc</a>')
+        yield '<a href="#">aaa</a>'
+        yield '<a href="#">bbb</a>'
+        yield '<a href="#">ccc</a>'
